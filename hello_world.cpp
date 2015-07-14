cead2787074f121d2921cf20e1044adfdcb255f0
1987wangsanguo <2396731957@qq.com>
2015-07-14 18:44:32 +0800

    add a cpp file

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <pcl/visualization/cloud_viewer.h>
+#include <iostream>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+    
+int user_data;  
+int 
+main ()
+{
+
+	std::cout<<"¼ÓÓÍ°¡"<<std::endl;
+    return 0;
+}
